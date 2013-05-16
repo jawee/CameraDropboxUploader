a7cc01e5f96eda820d5992102014cfd4255dc9a9
Andreas Olsson <olsson.andreas@gmail.com>
2013-05-16 20:04:16 +0200

    Initial Commit

---
+++ CameraDropboxUploader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CameraDropboxUploader
+//
+//  Created by Andreas Olsson on 5/16/13.
+//  Copyright (c) 2013 Andreas Olsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CameraDropboxUploader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CameraDropboxUploader
+//
+//  Created by Andreas Olsson on 5/16/13.
+//  Copyright (c) 2013 Andreas Olsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
